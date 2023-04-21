b286ceb1c6c2e24bb1e3802fb473030ee755042c
EverettBernard <124380095+EverettBernard@users.noreply.github.com>
2023-04-21 01:04:36 +0000

    finnished

--- js/script.js
+++ js/script.js
@@ -1,68 +1,51 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    
-    <!-- meta data -->
-    <meta charset="utf-8" />
-    <meta name="description" content="tempature" />
-    <meta name="keywords" content="immaculata, ics2o" />
-    <meta name="author" content="Everett" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="Questionmark" sizes="180x180" href="https://runningmagazine.ca/wp-content/uploads/2019/08/gettyimages-1002295536-170667a.jpg" />
-    <link rel="icon" type="image/png" sizes="32x32" href="https://runningmagazine.ca/wp-content/uploads/2019/08/gettyimages-1002295536-170667a.jpg" />
-    <link rel="icon" type="image/png" sizes="16x16" href="https://runningmagazine.ca/wp-content/uploads/2019/08/gettyimages-1002295536-170667a.jpg" />
-    <link rel="manifest" href="./fav_index/site.webmanifest" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-   <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.pink-red.min.css" />
-    <link rel="stylesheet" href="./css/style.css" />
-    
-    <!-- title of page and website-->
-    <title>Hot or Cold?</title>
-  </head>
-  <body>
-    
-    <!-- Mdl Components-->
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <script src="./js/script.js"></script>
-    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-      <header class="mdl-layout__header">
-        <div class="mdl-layout__header-row">
-          <span class="mdl-layout-title">Temperature calculator </span>
-        </div>
-      </header>
-      <main class="mdl-layout__content">
-        <center>
-         <h1>Is it Cold or Hot outside?</h1>
-        <img src="https://cdn-icons-png.flaticon.com/512/3534/3534501.png" alt="temperature" width="333" height="333">
-          
-        <!-- texfield -->
-        <form action="#">
-          <p>What is the Temperature?(&#8451;):</p>
-          <div class="mdl-textfield mdl-js-textfield">
-            <input
-              class="mdl-textfield__input"
-              type="number"
-              id="tempu"
-            />
-            <label class="mdl-textfield__label" for="tempU">Temperature</label>
-            <span class="mdl-textfield__error">Input is not a number</span>
-          </div>
-        </form>
-          
-        <!-- Tempature button -->
-        <button
-          class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-          onclick="calculate()"
-        >
-          Hot or Cold?
-        </button>
-        <br />
-        <p>
-            <div id="temp-display"></div>
-          </center>
-          </div>
-        </p>
-      </main>
-    </div>
-  </body>
-</html>
+// Created by: Everett
+// Created on: April 2023
+// This file contains the JS functions for index.html
+"use strict";
+
+/**
+ * This function calculates what movies the user can watch
+ */
+function calculate () {
+  
+  // user age input
+  let userage = parseFloat(document.getElementById('Uage').value);
+  let agestate = ""
+  // If statments for age
+ if (userage >=17) {
+
+        agestate = "You can watch any movie."
+
+    }
+
+    else if (userage >= 13) {
+
+        agestate = "You can watch PG-13 movies."
+
+    }  
+
+	else if (userage >=5) {
+
+        agestate = "You can watch PG movies."
+
+    }
+    else if (userage >0) {
+
+        agestate = "You can not watch any movies alone."
+    }
+    else if (userage <0) {
+
+        agestate = "Invalid age."
+    }
+
+  else {
+
+        agestate = "Invalid age."
+
+    }
+
+  // output what movies user can watch
+  document.getElementById('movie-display').innerHTML = agestate 
+
+}
+ 
